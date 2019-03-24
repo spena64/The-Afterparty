3658d0644de5af33a391070be9b5b8a7003ddee1
spena64 <spena6161@gmail.com>
2019-03-24 12:23:12 -0400

    yyipes

---
+++ Assets/Prototyping_Assets/Player_Scripts/PlayerHealth_Prototype.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class PlayerHealth_Prototype : MonoBehaviour
+{
+    public static int health;
+    public bool dead = false;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 1; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (health < 1)
+        {
+            dead = true; 
+            if(dead == true)
+            {
+                Debug.Log("Player has died.");
+            }
+            StartCoroutine("Die"); 
+        }
+    }
+
+    IEnumerator Die()
+    {
+        SceneManager.LoadScene("prototype_1");
+        yield return null; 
+    }
+}
---
+++ Assets/Prototyping_Assets/Player_Scripts/PlayerMoveBehavior_Prototype.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMoveBehavior_Prototype : MonoBehaviour
+{
+    public int playerSpeed = 10;
+    public int playerJump = 1250;
+    public float xMove;
+    public bool isAirborn;
+    public static char activeSide; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeSide = 't';
+        if (activeSide == 't')
+        {
+            Debug.Log("Active side set to Top"); 
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
+        SetActiveSide();
+    }
+
+    void MovePlayer()
+    {
+        // controls
+        xMove = Input.GetAxis("Horizontal");
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isAirborn == false)
+        {
+            Jump();
+            isAirborn = true;
+        }
+
+
+
+        // animation (maybe)
+
+        // active side (later)
+
+
+        // physics 
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(xMove * playerSpeed, gameObject.GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector2.up * playerJump);
+    }
+
+    void OnCollisionEnter2D (Collision2D col)
+    {
+        if(col.gameObject.tag == "Floor" || col.gameObject.tag == "Enemy")
+        {
+            isAirborn = false; 
+        }
+    }
+
+    void SetActiveSide()
+    {
+        if(Input.GetKeyDown(KeyCode.W))
+        {
+            activeSide = 't';
+            Debug.Log("Active side set to Top");
+        }
+        else if(Input.GetKeyDown(KeyCode.A))
+        {
+            activeSide = 'l';
+            Debug.Log("Active side set to Left");
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            activeSide = 'b';
+            Debug.Log("Active side set to Bottom");
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            activeSide = 'r';
+            Debug.Log("Active side set to Right");
+        }
+    } 
+}
